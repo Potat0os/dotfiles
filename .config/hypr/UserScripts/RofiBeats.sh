56b5baf08e12e8bcfba6aeb5de677514d00b4ca0
Potat0os <hxxd123game@gmail.com>
2026-02-01 11:53:08 +0400

    orgenized wallpapers

--- .config/hypr/UserScripts/RofiBeats.sh
+++ .config/hypr/UserScripts/RofiBeats.sh
@@ -53,7 +53,7 @@ play_local_music() {
     if [ "${filenames[$i]}" = "$choice" ]; then
       music_playing && stop_music
       notification "Now Playing:" "$choice"
-      mpv --no-video --playlist-start="$i" --loop-playlist --volume=35 "${local_music[@]}"
+      mpv --no-video --playlist-start="$i" --loop-playlist --volume=32 "${local_music[@]}"
       break
     fi
   done
@@ -63,7 +63,7 @@ play_local_music() {
 shuffle_local_music() {
   music_playing && stop_music
   notification "Shuffle Play local music"
-  mpv --no-video --shuffle --loop-playlist --volume=35 "$mDIR"
+  mpv --no-video --shuffle --loop-playlist --volume=23 "$mDIR"
 }
 
 # Play selected online music
